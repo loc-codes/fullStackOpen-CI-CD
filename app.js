04308a1eff4322a31f21e514db47f59485433962
Lachlan Young <lachie.s.young@gmail.com>
2023-12-02 10:17:42 +1100

    test

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ app.get('/health', (req, res) => {
   res.status(500).send('error')
 })
 
+//test
+
 app.listen(PORT, () => {
   // eslint-disable-next-line
   console.log(`server started on port ${PORT}`)
